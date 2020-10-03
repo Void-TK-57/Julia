27a41d66f80b71815f5a05207167d4397a6ff908
Pedro <pedroaa57@gmail.com>
2020-10-03 16:47:48 -0300

    Restrucutre functions

--- src/functools.jl
+++ src/functools.jl
@@ -2,28 +2,13 @@
 using IterTools
 using StatsBase
 
-# combinations of indices function
-comb(n_features, degree, include_bias) = chain([filter(issorted, Any[product(fill(0:n_features-1, n)...)...]) for n in Int(!include_bias):degree]...)
-
-function power_features(X::Array{T,2}, degree::Int64, include_bias::Bool=false) where {T <: Number}
-    # get number of sample and features
-    n_samples, n_features = size(X)
-    # calculate features combinations
-    combinations = comb(n_features, degree, include_bias)
-    # count output
-    output_size = count(_->true, combinations)
-    # allocate memory for the new matrix
-    X_ = Matrix{T}(undef, n_samples, output_size)
-    # for each combination,
-    for (i, c) in enumerate(combinations)
-        # set the column to the product
-        X_[:, i] = prod(X[:, Int[k+1 for k in c]], dims=2)
-    end
-    return X_
+# function to get all combinations with repetition up to the size passed (and whether it should inclue the empty set)
+function combination_with_repetition(array::Array{T,1}, size::Int64, include_empty_set::Bool)::Array{Tuple{Vararg{T,N} where N},1} where {T,N}
+    return [chain([filter(issorted, [product(fill(array, d)...)...]) for d in Int(!include_empty_set):size]...)...]
 end
 
-# feature name function
-function n_str(str, n)
+# function to get a feature name to the n power
+function feature_to_n(str, n)
     if n == 0
         return ""
     elseif n == 1
@@ -33,34 +18,22 @@ function n_str(str, n)
     end
 end
 
-function empty_dict(n::Int64)
-    # create empty dict
-    dict = Dict{Int64,Int64}()
-    # for each value, create key with 0
-    for k in 0:n-1
-        dict[k] = 0
+# function to replace a value of an array
+function replace(vector::Array{T,1}, old_value::T, new_value::T)::Array{T,1} where T
+    vector[vector.==old_value] .= new_value
+    # return the vector
+    return vector
+end
+
+# function to do a element wise multiplication
+function prod(itr, n::Int64=0, f::Function=x->x)
+    # initial value (for empty itr)
+    start = ones(n)
+    # for each value in itr do multiplication element wise apply the function
+    for i in itr
+        start = start .* f(i)
     end
-    return dict
+    return start
 end
 
-function features_names(features::Array{String,1}, degree::Int64, include_bias::Bool)::Array{String,1}
-    # function to get feature string based on the map
-    calculate_str(map_combination) = join( filter(x->x!="", [ n_str(features[i], map_combination[i-1]) for i in 1:length(features) ]), '.')
-    # get features based on the combinations
-    features = [ length([v...]) == 0 ? "1" : calculate_str( addcounts!(empty_dict(length(features)), [v...]) )  for v in comb(length(features), degree, include_bias) ]
-    # return the features
-    return features
-end
-
-# function to get min and max values of each column and each row
-function apply(m::Array{T,2}, f::Function, axis::Int64=1)::Array{Any,2} where {T <: Number}
-    if axis == 2
-        return [f(m[:, col]) for col in 1:size(m)[2]][:, :]
-    elseif axis == 1
-        return [f(m[row, :]) for row in 1:size(m)[1]][:, :]
-    else
-        throw(ArgumentError("Axis must be either 1 or 2"))
-    end
-end
-
-# terror_function(features, degree, include_bias) = [ length([v...]) == 0 ? "1" : join( filter(x->x!="", [ n_str(features[i], (addcounts!(Dict{Int64,Int64}(), [v...]))[i-1]) for i in 1:length(features) ]), '.')  for v in (chain([filter( issorted, Any[product(fill(0:length(features)-1, n)...)...]) for n in Int(!include_bias):degree]...) ) ] ]
+#power_features(features, degree, include_bias) = [ length([v...]) == 0 ? "1" : join( filter(x->x!="", [ n_str(features[i], (addcounts!(Dict{Int64,Int64}(), [v...]))[i-1]) for i in 1:length(features) ]), '.')  for v in chain([filter( issorted, Any[product(fill(0:length(features)-1, n)...)...]) for n in Int(!include_bias):degree]...)  ]
