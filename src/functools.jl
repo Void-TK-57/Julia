c4a7fde85eaa99421eff73f660127b5b0ff5d6b1
Pedro <pedroaa57@gmail.com>
2020-10-04 07:22:27 -0300

    Add is_nan function for other values other than Numbers

--- src/functools.jl
+++ src/functools.jl
@@ -2,6 +2,14 @@
 using IterTools
 using StatsBase
 
+# function to check if is nan, but for other types too
+is_nan(value)::Bool = false
+function is_nan(value::T)::Bool where {T <: Number}
+    return isnan(value)
+end
+
+
+
 # function to get all combinations with repetition up to the size passed (and whether it should inclue the empty set)
 function combination_with_repetition(array::Array{T,1}, size::Int64, include_empty_set::Bool)::Array{Tuple{Vararg{T,N} where N},1} where {T,N}
     return [chain([filter(issorted, [product(fill(array, d)...)...]) for d in Int(!include_empty_set):size]...)...]
@@ -19,8 +27,8 @@ function feature_to_n(str, n)
 end
 
 # function to replace a value of an array
-function replace(vector::Array{T,1}, old_value::T, new_value::T)::Array{T,1} where T
-    vector[vector.==old_value] .= new_value
+function replace(vector::Array{T,1}, old_value::U, new_value::V)::Array{T,1} where T where U where V
+    vector[ is_nan(old_value) ? is_nan.(vector) : vector .=== old_value] .= new_value
     # return the vector
     return vector
 end
